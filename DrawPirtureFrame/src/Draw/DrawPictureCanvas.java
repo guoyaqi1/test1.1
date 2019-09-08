250f9859a7eae4bbf68e2c216ed149a484afb615
guoyaqi <54883946+guoyaqi1@users.noreply.github.com>
2019-09-08 19:04:52 +0800

    Draw
    第一次用github创建Java  画图软件MFC

---
+++ DrawPirtureFrame/src/Draw/DrawPictureCanvas.java
@@ -0,0 +1,17 @@
+package Draw;
+
+import java.awt.image.BufferedImage;
+
+public class DrawPictureCanvas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setImage(BufferedImage image) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
